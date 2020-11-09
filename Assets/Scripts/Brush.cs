6e505607dfeef5705870216138f641d4985d8798
Apurv singh <apurvsingh9713@gmail.com>
2020-11-10 00:37:49 +0530

    Initial commit

---
+++ Assets/Scripts/Brush.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brush : MonoBehaviour
+{
+    public GameObject _terrain;
+    public Transform _brush;
+    public Shader drawShader;
+    public ParticleSystem particle;
+
+    private Material drawMaterial;
+    private Material myMaterial;
+    private RenderTexture splatmap;
+
+    [Range(0, 2)]
+    public float _brushSize;
+    [Range(0, 10)]
+    public float _brushStrength;
+
+    RaycastHit _groundHit;
+    int _layerMask;
+
+    private void Start()
+    {
+        particle.Stop();
+
+        _layerMask = LayerMask.GetMask("Ground");
+        drawMaterial = new Material(drawShader);
+        myMaterial = _terrain.GetComponent<MeshRenderer>().material;
+        myMaterial.SetTexture("_Splat", splatmap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat));
+
+    }
+
+    private void Update()
+    {
+        if (Physics.Raycast(_brush.position, -transform.right, out _groundHit, 1f, _layerMask))
+        {
+            drawMaterial.SetVector("_Coordinate", new Vector4(_groundHit.textureCoord.x, _groundHit.textureCoord.y, 0, 0));
+            drawMaterial.SetFloat("_Strength", _brushStrength);
+            drawMaterial.SetFloat("_Size", _brushSize);
+
+            RenderTexture temp = RenderTexture.GetTemporary(splatmap.width, splatmap.height, 0, RenderTextureFormat.ARGBFloat);
+            Graphics.Blit(splatmap, temp);
+            Graphics.Blit(temp, splatmap, drawMaterial);
+            RenderTexture.ReleaseTemporary(temp);
+
+            particle.Play();
+        }
+        else
+        {
+            particle.Stop();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(_brush.position, -transform.right * 1f);
+    }
+}
---
+++ Assets/Scripts/DrawWithMouse.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class DrawWithMouse : MonoBehaviour
+{
+    public Camera _camera;
+    public Shader _drawShader;
+
+    private RenderTexture _splatmap;
+    private Material _carvingMaterial, _drawMaterial;
+    private RaycastHit _hit;
+    
+    [Range(0,10)]
+    public float _brushSize;
+    [Range(0,10)]
+    public float _brushStrength;
+
+    private void Start()
+    {
+        _drawMaterial = new Material(_drawShader);
+        _drawMaterial.SetVector("_Color", Color.red);
+
+        _carvingMaterial = GetComponent<MeshRenderer>().material;
+        _splatmap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        _carvingMaterial.SetTexture("_Splat", _splatmap);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out _hit))
+            {
+                _drawMaterial.SetVector("_Coordinate", new Vector4(_hit.textureCoord.x, _hit.textureCoord.y, 0, 0));
+                _drawMaterial.SetFloat("_Strength", _brushStrength);
+                _drawMaterial.SetFloat("_Size", _brushSize);
+
+                RenderTexture temp = RenderTexture.GetTemporary(_splatmap.width, _splatmap.height, 0, RenderTextureFormat.ARGBFloat);
+                Graphics.Blit(_splatmap, temp);
+                Graphics.Blit(temp, _splatmap, _drawMaterial);
+                RenderTexture.ReleaseTemporary(temp);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 256, 256), _splatmap, ScaleMode.ScaleToFit, false, 1);
+    }
+}
